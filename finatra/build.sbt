ea69dead75c181f9c71e02f9968318048e9125ea
James Earl Douglas <james@earldouglas.com>
2012-09-14 16:17:02 -0700

    add finatra example

---
+++ finatra/build.sbt
@@ -0,0 +1,11 @@
+name := "finatra-scamper"
+
+resolvers += "repo.juliocapote.com" at "http://repo.juliocapote.com"
+
+resolvers += "scala-tools.org" at "http://scala-tools.org/repo-releases"
+
+resolvers += "repo.codahale.com" at "http://repo.codahale.com"
+
+libraryDependencies += "com.twitter" % "finatra" % "0.1.7"
+
+
---
+++ finatra/src/main/scala/scamper/Scamper.scala
@@ -0,0 +1,26 @@
+package scamper
+
+import com.twitter.finatra.Controller
+import com.twitter.finatra.FinatraServer
+
+object Scamper extends App {
+  val scamper = new Scamper
+
+  FinatraServer.register(scamper)
+  FinatraServer.start()
+}
+
+class Scamper extends Controller {
+
+  get("/fast") { request => render.body("<h1>slept for %d ms</h1>".format(sleep(0))).header("Content-Type", "text/html") }
+  get("/medium") { request => render.body("<h1>slept for %d ms</h1>".format(sleep(150))).header("Content-Type", "text/html") }
+  get("/slow") { request => render.body("<h1>slept for %d ms</h1>".format(sleep(300))).header("Content-Type", "text/html") }
+
+  def sleep(ms: Long): Long = {
+    val start = System.currentTimeMillis
+    Thread.sleep(ms)
+    val stop = System.currentTimeMillis
+    stop - start
+  }
+}
+
