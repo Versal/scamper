e4302f694d0175a46bfbb16a79f6da69c00bb06b
James Earl Douglas <james@earldouglas.com>
2012-11-27 09:36:39 -0800

    use the cluster module for multicore goodness

--- node.js/app.js
+++ node.js/app.js
@@ -29,14 +29,27 @@ app.get('/medium', scamper.medium);
 //sleep 300ms
 app.get('/slow', scamper.slow);
 
-/**
- * Run Node.js Server
- */
-var server = http.createServer(app).listen(9000, '127.0.0.1', function(){
-    /**
-     * Start Message
-     */
+
+// cluster module
+// http://nodejs.org/api/cluster.html#cluster_cluster
+
+var cluster = require('cluster');
+var http = require('http');
+var numCPUs = require('os').cpus().length;
+
+if (cluster.isMaster) {
+  // Fork workers.
+  for (var i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', function(worker, code, signal) {
+    console.log('worker ' + worker.process.pid + ' died');
+  });
+} else {
+  // Workers can share any TCP connection
+  // In this case its a HTTP server
+  http.createServer(app).listen(9000, '127.0.0.1', function(){
     console.log("Node.js Web server listening on port 9000");
-});
-
-
+  });
+}
